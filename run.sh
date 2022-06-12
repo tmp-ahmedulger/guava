de212c0844ae582804c77ac6e6a527dd64f34081
ahulger <ahulger@ebay.com>
2022-06-12 15:29:50 +0300

    Updated permission checking structure

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
+
+echo "api-utils: Re-bulding and pushing to maven local"
 ./api-utils/gradlew clean build publishToMavenLocal -p ./api-utils
 
+echo "user-manager: Re-bulding and pushing to maven local"
 ./user-manager/gradlew clean build publishToMavenLocal -p ./user-manager
 
+echo "micro-services-cloud: Re-bulding"
 ./microservices-cloud/gradlew clean build -p ./microservices-cloud
 
+echo "parcel-delivery-service: Re-bulding"
 ./parcel-delivery-service/gradlew clean build -p ./parcel-delivery-service
 
-./docker-compose up -d
+echo "Applications running up on docker environemnt"
+docker-compose up -d
