2afd564b4446798467e65904ad465f0d3869a4bb
ahulger <ahulger@ebay.com>
2022-06-12 13:15:13 +0300

    Renamed classes by deleting parcel prefixa

--- parcel-delivery-service/src/main/java/com/ulger/guava/parceldeliveryservice/api/parcel/operation/update/status/consent/filter/StatusConsentFilter.java
+++ parcel-delivery-service/src/main/java/com/ulger/guava/parceldeliveryservice/api/parcel/operation/update/status/consent/filter/StatusConsentFilter.java
@@ -1,6 +1,7 @@
 package com.ulger.guava.parceldeliveryservice.api.parcel.operation.update.status.consent.filter;
 
 import com.ulger.guava.parceldeliveryservice.api.ApiErrorCode;
+import com.ulger.guava.parceldeliveryservice.api.ApiException;
 import com.ulger.guava.parceldeliveryservice.api.consent.ConsentFilterException;
 import com.ulger.guava.parceldeliveryservice.api.parcel.Status;
 import lombok.extern.slf4j.Slf4j;
@@ -16,11 +17,11 @@ public class StatusConsentFilter implements StatusUpdateConsentFilter {
     public void check(StatusUpdateConsentFilterParams params) {
 
         Status sourceStatus = params.getParcel().getStatus();
-        Status targetStatus = params.getParcel().getStatus();
+        Status targetStatus = params.getStatusUpdateDto().getStatus();
 
         if (sourceStatus == targetStatus) {
             log.info("Statuses are same, skipping update. sourceLStatus={}, targetStatus={}", sourceStatus, targetStatus);
-            throw new ConsentFilterException(ApiErrorCode.SAME_STATUS.getKey());
+            throw new ConsentFilterException(new ApiException(ApiErrorCode.SAME_STATUS.getKey()));
         }
     }
 }
